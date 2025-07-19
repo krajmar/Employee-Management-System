447fd2bd18d2824f9b93aad33132b6a503d24189
Mario Krajoski <krajoskimario@gmail.com>
2025-07-19 17:09:35 +0200

    frontend changes login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './Components/Login.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
-      App
+      <Login/>
     </>
   )
 }
